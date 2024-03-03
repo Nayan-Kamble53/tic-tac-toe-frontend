62e3aec74f5e413486b5575c2ee09b6d84191e8c
Nayan-Kamble53 <nayankamble712@gmail.com>
2024-03-03 19:11:37 +0530

    changes in url

--- src/App.js
+++ src/App.js
@@ -132,7 +132,7 @@ const App = () => {
     const username = result.value;
     setPlayerName(username);
 
-    const newSocket = io("http://localhost:5000", {
+    const newSocket = io("https://tic-tac-toe-multiplayer-backend.onrender.com", {
       autoConnect: true,
     });
 
